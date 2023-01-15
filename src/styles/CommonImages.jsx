e1ef243001a8e60608319e43b5bb16a96e30ebe5
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-01-16 01:45:22 +0900

    assets: 감정키워드에 사용할 이미지파일 추가 (#3)

---
+++ src/styles/CommonImages.jsx
@@ -0,0 +1,29 @@
+import EMBARRASSMENT_ON_EMOTION from '../assets/images/당황스러워요_on.png';
+import EMBARRASSMENT_OFF_EMOTION from '../assets/images/당황스러워요_off.png';
+import BAD_ON_EMOTION from '../assets/images/별로에요_on.png';
+import BAD_OFF_EMOTION from '../assets/images/별로에요_off.png';
+import SAD_ON_EMOTION from '../assets/images/속상해요_on.png';
+import SAD_OFF_EMOTION from '../assets/images/속상해요_off.png';
+import WEIRD_ON_EMOTION from '../assets/images/이상해요_on.png';
+import WEIRD_OFF_EMOTION from '../assets/images/이상해요_off.png';
+import GOOD_ON_EMOTION from '../assets/images/좋아요_on.png';
+import GOOD_OFF_EMOTION from '../assets/images/좋아요_off.png';
+import NOTBAD_ON_EMOTION from '../assets/images/평범해요_on.png';
+import NOTBAD_OFF_EMOTION from '../assets/images/평범해요_off.png';
+import CROWN from '../assets/images/crown.png';
+
+export {
+  EMBARRASSMENT_ON_EMOTION,
+  EMBARRASSMENT_OFF_EMOTION,
+  BAD_ON_EMOTION,
+  BAD_OFF_EMOTION,
+  SAD_ON_EMOTION,
+  SAD_OFF_EMOTION,
+  WEIRD_ON_EMOTION,
+  WEIRD_OFF_EMOTION,
+  GOOD_ON_EMOTION,
+  GOOD_OFF_EMOTION,
+  NOTBAD_ON_EMOTION,
+  NOTBAD_OFF_EMOTION,
+  CROWN,
+};
