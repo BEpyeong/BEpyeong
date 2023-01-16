96b38a35b4e8c3ef260e5a6ba9bd4913bfb12a51
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-01-16 18:25:09 +0900

    feat: 상태관리를 통하여 별점 관리하는 기능 추가 (#8)

--- src/components/common/Buttons/StarRateButton.jsx
+++ src/components/common/Buttons/StarRateButton.jsx
@@ -2,12 +2,29 @@ import * as React from 'react';
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import styled from 'styled-components';
+import { useState } from 'react';
+
+const getLabelText = (value) => {
+  return `${value} Star${value !== 1 ? 's' : ''}, ${value}`;
+};
 
 const StarRateButton = () => {
   // 읽는 기능만 존재할 경우 name='half-rating-read' 하고 readOnly props 추가
+  const [value, setValue] = useState(0);
+  const [hover, setHover] = useState(-1);
+
   return (
     <StyleStack spacing={1}>
-      <StyleRating name='half-rating' defaultValue={0.5} precision={0.5} />
+      <StyleRating
+        name='half-rating'
+        defaultValue={0.5}
+        precision={0.5}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        value={value}
+      />
+      {value}
     </StyleStack>
   );
 };
