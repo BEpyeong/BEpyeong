898d5d449abe55ddc90d8bc9d3e5d70c87670a0f
minzyee <vvzmsqufvv@gmail.com>
2023-01-17 17:56:26 +0900

    assets: 알림함 및 신고 접수 관리에 사용할 아이콘 추가 (#11)

--- src/styles/CommonIcons.jsx
+++ src/styles/CommonIcons.jsx
@@ -4,5 +4,18 @@ import FAVORITES_ON_ICON from '../assets/icons/관심목록_on.png';
 import FAVORITES_OFF_ICON from '../assets/icons/관심목록_off.png';
 import MYPAGE_ON_ICON from '../assets/icons/마이페이지_on.png';
 import MYPAGE_OFF_ICON from '../assets/icons/마이페이지_off.png';
+import NOTICE_REPORT from '../assets/icons/알림목록_신고.png';
+import NOTICE_EVALUATE from '../assets/icons/알림목록_작성한글평가.png';
+import NOTICE_ASSESSMENT_CLOSING from '../assets/icons/알림목록_평가한글마감.png';
 
-export { HOME_ON_ICON, HOME_OFF_ICON, FAVORITES_ON_ICON, FAVORITES_OFF_ICON, MYPAGE_ON_ICON, MYPAGE_OFF_ICON };
+export {
+  HOME_ON_ICON,
+  HOME_OFF_ICON,
+  FAVORITES_ON_ICON,
+  FAVORITES_OFF_ICON,
+  MYPAGE_ON_ICON,
+  MYPAGE_OFF_ICON,
+  NOTICE_REPORT,
+  NOTICE_EVALUATE,
+  NOTICE_ASSESSMENT_CLOSING,
+};
