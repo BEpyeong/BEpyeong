d54cfaa7d8f17918c0ad43da334b50eb5d8fb8c4
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-01-19 02:00:36 +0900

    feat: 타이틀에 맞게 페이지 이동하도록 구현 (#17)

--- src/components/common/Buttons/BasicIconButton.jsx
+++ src/components/common/Buttons/BasicIconButton.jsx
@@ -1,25 +1,54 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { SEARCH_ICON, BACK_ICON, ALARM_ICON, CHECK_ICON, HAMBURGER_ICON } from 'styles/CommonIcons.jsx';
 
-const BasicIconButton = ({ onClickPageHandler, title }) => {
+const BasicIconButton = ({ title }) => {
+  const navigate = useNavigate();
+
   const iconImgList = [
     { iconTitle: '검색', img: SEARCH_ICON },
-    { iconTitle: '알림', img: BACK_ICON },
-    { iconTitle: '뒤로가기', img: ALARM_ICON },
-    { iconTitle: '햄버거', img: CHECK_ICON },
-    { iconTitle: '체크', img: HAMBURGER_ICON },
+    { iconTitle: '알림', img: ALARM_ICON },
+    { iconTitle: '뒤로가기', img: BACK_ICON },
+    { iconTitle: '햄버거', img: HAMBURGER_ICON },
+    { iconTitle: '체크', img: CHECK_ICON },
   ];
 
   const iconDataObj = iconImgList.find(({ iconTitle }) => {
     return iconTitle === title;
   });
 
+  const moveSelectPage = (title) => {
+    switch (title) {
+      case '검색':
+        navigate('/');
+        break;
+
+      case '알림':
+        navigate('/');
+        break;
+
+      case '뒤로가기':
+        navigate('/');
+        break;
+
+      case '햄버거':
+        navigate('/');
+        break;
+
+      case '체크':
+        navigate('/');
+        break;
+
+      default:
+    }
+  };
+
   return (
     <>
       {iconDataObj && (
-        <StyleIconBtn onClick={onClickPageHandler}>
+        <StyleIconBtn onClick={() => moveSelectPage(title)}>
           <img src={iconDataObj.img} alt={iconDataObj.iconTitle + ' 아이콘'} />
         </StyleIconBtn>
       )}
@@ -32,4 +61,7 @@ export default BasicIconButton;
 const StyleIconBtn = styled.button`
   width: 24px;
   height: 24px;
+  & + & {
+    margin-left: 10px;
+  }
 `;
