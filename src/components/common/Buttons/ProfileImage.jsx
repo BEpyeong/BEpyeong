6676f147c56a78d1aca1424904714efcfd151b81
minzyee <vvzmsqufvv@gmail.com>
2023-01-20 18:33:18 +0900

    move: ProfileImage.jsx를 Buttons 폴더의 하위로 변경 (#25)

---
+++ src/components/common/Buttons/ProfileImage.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PROFILE_UPLOAD } from 'styles/CommonIcons';
+import { PROFILE_DEFAULT } from 'styles/CommonImages';
+
+const ProfileImage = ({ src, alt = '', borderWeight = '4' }) => {
+  return (
+    <>
+      <label htmlFor='profileImg'>
+        <StyleProfileWrapper>
+          <UserProfileImage src={PROFILE_DEFAULT} alt={alt} width={`110`} borderWeight={borderWeight} />
+          <UploadFileIcon src={PROFILE_UPLOAD} />
+        </StyleProfileWrapper>
+      </label>
+      <input className='sr-only' type='file' id='profileImg' accept='image/*' />
+    </>
+  );
+};
+
+export default ProfileImage;
+
+const StyleProfileWrapper = styled.div`
+  position: relative;
+  width: 110px;
+  height: 110px;
+  margin: 0 auto 30px;
+`;
+
+const UserProfileImage = styled.img`
+  width: 110px;
+  height: 110px;
+  border: ${(props) => props.borderWeight}px solid var(--border-color);
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const UploadFileIcon = styled.img`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 36px;
+  height: 36px;
+  z-index: 10;
+  cursor: pointer;
+`;
