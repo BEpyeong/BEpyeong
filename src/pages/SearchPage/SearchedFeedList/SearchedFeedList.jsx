c8073470d185ad6038109aafa5dce6e74d08d312
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-01-24 16:42:04 +0900

    design: 검색한 키워드에 맞는 피드리스트 페이지 퍼블리싱 (#45)

---
+++ src/pages/SearchPage/SearchedFeedList/SearchedFeedList.jsx
@@ -0,0 +1,8 @@
+import FeedList from 'components/common/FeedList/FeedList';
+import React from 'react';
+
+const SearchedFeedList = () => {
+  return <FeedList />;
+};
+
+export default SearchedFeedList;
